f553b45907221a894cdfc728a155e2b46b688123
Ali Nahid <alinahid@gmail.com>
2022-07-01 22:58:01 +1000

    wip working

--- src/main/java/local/simplegreetings/Controllers/api/GreetingsController.java
+++ src/main/java/local/simplegreetings/Controllers/api/GreetingsController.java
@@ -2,11 +2,12 @@ package local.simplegreetings.Controllers.api;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import local.simplegreetings.AppProperties;
 import local.simplegreetings.Models.GreetingResponseObject;
 
 
@@ -18,14 +19,20 @@ public class GreetingsController {
     private final Logger logger = LoggerFactory.getLogger(GreetingsController.class);
 
 
-    @Value("${app.greeting-text}")
-    private String greetingText;
+    @Autowired
+    private AppProperties appProperties;
 
+
+    @GetMapping("/healthcheck")
+    public GreetingResponseObject healthCheck() {
+        logger.info("Inside healthcheck..");
+        return new GreetingResponseObject("running");
+    }
 
 
     @GetMapping("/do")
     public GreetingResponseObject greeting() {
         logger.info("Inside greeting..");
-        return new GreetingResponseObject(this.greetingText);
+        return new GreetingResponseObject(this.appProperties.getGreetingText());
     } 
 }
--- src/test/java/local/simplegreetings/SimplegreetingsApplicationTests.java
+++ src/test/java/local/simplegreetings/SimplegreetingsApplicationTests.java
@@ -1,13 +1,37 @@
 package local.simplegreetings;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-@SpringBootTest
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+
+import local.simplegreetings.Controllers.api.GreetingsController;
+
+@SpringBootTest (webEnvironment = WebEnvironment.RANDOM_PORT)
 class SimplegreetingsApplicationTests {
+
+	@Autowired
+	private GreetingsController controller;
+
+	@Autowired
+	private TestRestTemplate restTemplate;
+
+	@LocalServerPort
+	private int port;
 
 	@Test
 	void contextLoads() {
+		assertNotNull(controller);
 	}
+
+	@Test
+    public void shouldPassIfStringMatches() throws Exception {
+        assertTrue(restTemplate.getForObject("http://localhost:" + port + "/api/greetings/healthcheck", String.class).contains("running"));
+    }
 
 }
